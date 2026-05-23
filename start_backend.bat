bb58978dd13fd35d1673e499e98fb7f183a9b6d9
SaurabhDvdi <saurabhdwivedi426@gmail.com>
2026-05-23 20:30:43 +0530

    Remove ignored files from repository

--- start_backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd backend
-python -m venv venv
-call venv\Scripts\activate
-pip install -r requirements.txt
-python main.py
-
-http://localhost:8000/redoc --> to see Cleaner Documentation View
-http://localhost:8000/docs --> in case you want to try out each end point refere to Swagger UI
